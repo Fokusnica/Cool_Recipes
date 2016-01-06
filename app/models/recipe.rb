366debbb158e0f65c107a26b3e976e06cbff3b95
Derevianko Liubov <dereviankoliubov@gmail.com>
2016-01-06 22:05:07 +0200

    creating associations - recipes and categories

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,6 @@
 class Recipe < ActiveRecord::Base
-DIFFICULTY = %w(Easy Medium Hard)
+  belongs_to :food_type
+  belongs_to :food_preference
+  belongs_to :cuisines
+  DIFFICULTY = %w(Easy Medium Hard)
 end
